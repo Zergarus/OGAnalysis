cb49f445e7bf6200395265b3b956a12cee475f3a
Zergarus <47299260+Zergarus@users.noreply.github.com>
2022-07-03 22:59:38 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("generateAndPlot").hover(function() {
-        var p = $("generateAndPlot").css("background", "linear-gradient(0deg, #3111e779 0%, hsla(0, 0%, 100%, 0.445) 51%, #ffffff65 100%)");
-        p.hide(1500).show(1500);
-        p.queue(function() {
-          p.css("background", "linear-gradient(0deg, #ffffff79 0%, hsla(0, 0%, 100%, 0.445) 51%, #ffffff65 100%)");
-        });
-      });
-})
